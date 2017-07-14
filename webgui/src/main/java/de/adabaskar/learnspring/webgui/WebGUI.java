5a4831f4bfd78ecc08a600b4bc3fd81369b49917
Bogumil Bartczak <bogumil.bartczak@gmx.de>
2017-07-14 21:46:43 +0200

    smoking webgui with spring boot + thymleaf

---
+++ webgui/src/main/java/de/adabaskar/learnspring/webgui/WebGUI.java
@@ -0,0 +1,15 @@
+package de.adabaskar.learnspring.webgui;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebGUI {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(WebGUI.class, args);
+
+	}
+
+}
